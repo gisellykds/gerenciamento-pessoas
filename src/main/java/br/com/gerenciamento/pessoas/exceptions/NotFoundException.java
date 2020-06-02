d457c0ff42b43e46af207a232aa4e3f38639542d
Giselly K D Silva <giselly@ibm.com>
2020-06-01 22:31:52 -0300

    Limpeza de código, refatorando método de verificação de empty list

--- src/main/java/br/com/gerenciamento/pessoas/exceptions/NotFoundException.java
+++ src/main/java/br/com/gerenciamento/pessoas/exceptions/NotFoundException.java
@@ -1,9 +1,15 @@
 package br.com.gerenciamento.pessoas.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Nenhum cadastro encontrado.")
+@Getter
 public class NotFoundException extends RuntimeException{
 
+    public NotFoundException(){
+        super("Nenhum cadastro encontrado.");
+    }
 }

0db54f9937f71bb2944766cf21c02d50fce56b04
gisellykds <gisellykathellynsilva@gmail.com>
2020-06-19 22:32:19 -0300

    Atualizando mensagem de exception para inglês.

--- src/main/java/br/com/gerenciamento/pessoas/exceptions/NotFoundException.java
+++ src/main/java/br/com/gerenciamento/pessoas/exceptions/NotFoundException.java
@@ -3,9 +3,8 @@ package br.com.gerenciamento.pessoas.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Not Found")
 public class NotFoundException extends RuntimeException{
     public NotFoundException(){
-        super("Nenhum cadastro encontrado.");
+        super("No registration found.");
     }
 }
